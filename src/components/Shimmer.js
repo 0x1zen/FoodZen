f36233f9c92c212439d52cb56869e4e4f069e52f
Raj Dubal <126544333+0x1zen@users.noreply.github.com>
2025-01-31 07:39:51 +0000

    Added Shimmer Effect for cards

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,14 +1,14 @@
 const Shimmer = () => {
   return (
     <div className="shimmer-container">
-      <div className="shimmer-cards">Card</div>
-      <div className="shimmer-cards">Card</div>
-      <div className="shimmer-cards"></div>
-      <div className="shimmer-cards"></div>
-      <div className="shimmer-cards"></div>
-      <div className="shimmer-cards"></div>
-      <div className="shimmer-cards"></div>
-      <div className="shimmer-cards"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
     </div>
   );
 };
