9827a3e8e5c35bc2f8923eabdcf837815df7e624
Raj Dubal <23460898-RajDubal@users.noreply.replit.com>
2025-02-07 05:39:14 +0000

    removed extra unnecessary component

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,14 @@
-import User from "./User.js";
 import UserClass from "./UserClass.js";
 import React, { Component } from "react";
 
 class About extends Component {
   constructor(props) {
     super(props);
-    console.log(" parent constructor callled");
-  }
-  componentDidMount() {
-    console.log(" parent componentDidMount callled");
-  }
-  componentDidUpdate(){
-    console.log(" parent componentDidUpdate callled");
   }
   render() {
-    console.log(" parent render callled");
     return (
       <div>
-        <h1>About Us</h1>
-        <User name={"Raj Dubal"} location="Mumbai" contact={"@0x1zen"} />
-        <UserClass name={"Raj Dubal"} location="Mumbai" contact={"@0x1zen"} />
+        <UserClass/>
       </div>
     );
   }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,7 +4,6 @@ import { GITHUB_API } from "../utils/constants";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(" child constructor callled");
     this.state = {
       userInfo: {
         name: "dummy name",
@@ -17,24 +16,15 @@ class UserClass extends React.Component {
     const data = await fetch(GITHUB_API);
     const response = await data.json();
     this.setState({ userInfo: response });
-    console.log(" child componentDidMount callled");
-  }
-  componentDidUpdate() {
-    console.log(" child component did update called");
-  }
-  componentWillUnmount() {
-      console.log("Component WIll unmount called");
   }
 
   render() {
-    const { name, location, bio, avatar_url } = this.state.userInfo;
-    console.log(" child page render called");
+    const { login, bio, avatar_url } = this.state.userInfo;
     return (
       <div className="user-card">
         <img src={avatar_url}></img>
-        <h1>Name : {name}</h1>
-        <h2>location : {location}</h2>
-        <h3>bio: {bio}</h3>
+        <h1>Name : {login}</h1>
+        <h3>{bio}</h3>
       </div>
     );
   }
